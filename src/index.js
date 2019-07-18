4d20c79677a7f5a5b35e3f5ec8cbcf2f31415bde
Amir Momenian <nainemom@gmail.com>
2019-07-19 00:36:43 +0430

    add new sections

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import '../styles/style.css'
-import header from './parts/header.htm'
-import objective from './parts/objective.htm'
+import '../styles/style.scss'
+
 const $app = document.getElementById('app')
 
-
+const header = require('./parts/header.htm')
 $app.innerHTML += header
 
+const objective = require('./parts/objective.htm')
 const start = 1367350200000
 const end = new Date().getTime()
 const exp = Math.round((end - start) / 31536000000)
 $app.innerHTML += objective.replace('{{experience}}', exp)
+
+const skills = require('./parts/skills.htm')
+$app.innerHTML += skills
+
+const workExprience = require('./parts/work-exprience.htm')
+$app.innerHTML += workExprience
 
 document.dispatchEvent(new Event('prerender-ready'))
