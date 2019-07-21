c66dae2b6f8d71cc200fc16cd3b07efcccb700bd
nainemom <nainemom@gmail.com>
2019-07-21 16:56:42 +0430

    create template file

---
+++ src/templates.js
@@ -0,0 +1,32 @@
+const self = {
+  workExpTag(item) {
+    return `
+      <div class="list-section margin-v padding-h">
+        <div>
+          <h3 class="inline-block"> ${item.title} </h3> - <i> From ${self.dateFormat(item.from)} To ${self.dateFormat(item.to)} (${item.type === 1 ? 'Full-Time' : 'Part-Time'}) </i>
+        </div>
+        <h4> ${item.position} </h4>
+        <p> ${item.description} </p>
+      </div>
+    `
+  },
+  openSourceProjectTag(item) {
+    return `
+      <div class="list-section margin-v padding-h">
+        <div>
+          <h3 class="inline-block"> ${item.title} </h3> - <i> as ${item.as === 0 ? 'Maintainer' : 'Contributer'}  </i> - <a href="${item.link}" target="_blank">Access Link</a>
+        </div>
+        <p> ${item.description} </p>
+      </div>
+    `
+  },
+  dateFormat(date) {
+    if (date === 'present') {
+      return 'Present'
+    } else {
+      return `${date.getMonth() + 1}-${date.getFullYear()}`
+    }
+  }
+}
+
+export default self
