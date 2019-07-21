e037c43ad94d7091fd2955e1b811c8845cc1c541
nainemom <nainemom@gmail.com>
2019-07-21 12:27:04 +0430

    add workExprience section

--- src/me.js
+++ src/me.js
@@ -4,5 +4,41 @@ export default {
     const start = new Date(this.careerStart).getTime()
     const end = new Date().getTime()
     return `~${Math.round((end - start) / 31536000000)}`
-  }
+  },
+  workExprience: [
+    {
+      title: 'Snapp',
+      from: new Date('May 2019'),
+      to: 'present',
+      type: 1
+    },
+    {
+      title: 'Paygear Tunisia',
+      from: new Date('November 2018'),
+      to: new Date('April 2019'),
+      type: 0,
+      position: 'Frontend Developer'
+    },
+    {
+      title: 'HotelOneClick',
+      from: new Date('March 2018'),
+      to: new Date('April 2019'),
+      type: 0,
+      position: 'Frontend Developer - Platform Designer'
+    },
+    {
+      title: 'Paygear',
+      from: new Date('April 2017'),
+      to: new Date('April 2019'),
+      type: 1,
+      position: 'Frontend Developer'
+    },
+    {
+      title: 'Mahna',
+      from: new Date('May 2013'),
+      to: new Date('April 2017'),
+      type: 1,
+      position: 'Full-stack Developer'
+    },
+  ]
 }
