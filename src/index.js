30ac5b27eb621675a5aadb632163a0317b3bf414
nainemom <nainemom@gmail.com>
2019-07-21 12:26:51 +0430

    change importing style

--- src/index.js
+++ src/index.js
@@ -3,16 +3,11 @@ import me from './me.js'
 
 const $app = document.getElementById('app')
 
-const header = require('./parts/header.htm')
-$app.innerHTML += header
+$app.innerHTML += require('./parts/header.htm')
+$app.innerHTML += require('./parts/objective.htm')
+$app.innerHTML += require('./parts/skills.htm')
+$app.innerHTML += require('./parts/work-exprience.htm')
 
-const objective = require('./parts/objective.htm')
-$app.innerHTML += objective.replace('{{experience}}', me.experience())
-
-const skills = require('./parts/skills.htm')
-$app.innerHTML += skills
-
-const workExprience = require('./parts/work-exprience.htm')
-$app.innerHTML += workExprience
+$app.innerHTML = $app.innerHTML.replace('{{experience}}', me.experience())
 
 document.dispatchEvent(new Event('prerender-ready'))
