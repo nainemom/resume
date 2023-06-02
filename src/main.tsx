89ceab5ab75846b87cd21aa8ff9044214c820512
Amir Momenian <nainemom@gmail.com>
2023-06-02 21:06:48 +0330

    redirect on gh pages

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from '@/App'
 import '@/index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+if (window.location.hostname === 'nainemom.github.io') {
+  window.location.href = 'https://resume.nainemom.com';
+} else {
+  ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
+}
