d92d5ae4932e698035dbf7c94876a388947144f2
Amir Momenian <nainemom@gmail.com>
2023-06-02 21:10:47 +0330

    fix github.io redirect

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from '@/App'
 import '@/index.css'
 
-if (window.location.hostname === 'nainemom.github.io') {
-  window.location.href = 'https://resume.nainemom.com';
-} else {
-  ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-  )
-}
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
