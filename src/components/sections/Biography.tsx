60d25120ae5ad539a4dee72c77924cee3ef03030
Amir Momenian <nainemom@gmail.com>
2024-03-26 13:05:26 +0330

    update mitra bio

--- src/components/sections/Biography.tsx
+++ src/components/sections/Biography.tsx
@@ -11,8 +11,8 @@ export default function Biography(props: BiographyProps) {
   return (
     <div className={cx(props.className)}>
       <h1 className="text-4xl font-extrabold"> { props.objective.name } </h1>
-      <p className="text-lg text-gray-700 font-semibold mb-4">{ props.objective.role }</p>
-      <Markdown className="text-base text-start">
+      {/* <p className="text-lg text-gray-700 font-semibold mb-4">{ props.objective.role }</p> */}
+      <Markdown className="text-base text-start mt-6">
         { props.objective.info }
       </Markdown>
     </div>
