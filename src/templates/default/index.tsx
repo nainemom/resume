b2ab13f7481d8e6c5a2b14affdda0bf3ec6931f7
Amir Momenian <nainemom@gmail.com>
2025-07-09 15:15:02 +0330

    add photo on desktop

--- src/templates/default/index.tsx
+++ src/templates/default/index.tsx
@@ -14,7 +14,7 @@ export default function DefaultTemplate({
 	return (
 		<Layout className="grid grid-cols-1 lg:grid-cols-3 print:grid-cols-3 gap-3 p-3 lg:p-0 print:p-0">
 			<Column className="contents lg:block print:block col-span-1 space-y-3 p-3 bg-zinc-100">
-				<Section className="order-1 lg:hidden print:hidden text-center">
+				<Section className="order-1 lg:order-none text-center">
 					<Picture objective={objective} />
 				</Section>
 				<Section className="order-2 lg:order-none print:order-none text-center lg:text-start print:text-start">
